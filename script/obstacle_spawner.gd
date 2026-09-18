92a8cc1c71ac4a3c49fc270cc8600191fc3c5ba6
KKDowngrade <spranginaiteskim@gmail.com>
2026-09-18 11:49:05 -0300

    casa 1
    mudança nos sprites, variação de altura

--- script/obstacle_spawner.gd
+++ script/obstacle_spawner.gd
@@ -2,8 +2,11 @@ extends Marker2D
 
 const OBSTACLE = preload("uid://bslrrkxu87f06")
 
-
+@export var altura_minima: float = -150.0
+@export var altura_maxima: float = 150.0
 
 func _on_timer_timeout() -> void:
 	var instance := OBSTACLE.instantiate()
+	var altura_aleatoria = randf_range(altura_minima, altura_maxima)
+	instance.position.y = altura_aleatoria
 	add_child(instance)
